4bf2487b8fb15b7b262747cb5c00a8fc9266e418
Piyush Eklavya <piyusheklavya7@gmail.com>
2022-06-26 13:19:21 +0530

    sign out handler

--- auth/src/sign-out.ts
+++ auth/src/sign-out.ts
@@ -5,7 +5,9 @@ const router = Router();
 
 router.post('/api/users/signout', (request, response) => {
     console.log('GET /api/users/signout');
-    response.send('GET /api/users/signout');
+
+    request.session = null;
+    response.send({});
 });
 
 export { router as signoutRouter };
