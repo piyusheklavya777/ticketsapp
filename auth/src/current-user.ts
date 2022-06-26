c7b727b281d5015cf3f73f18abe4412a76ae43af
Piyush Eklavya <piyusheklavya7@gmail.com>
2022-06-26 13:18:28 +0530

    use middlewares in current user service

--- auth/src/current-user.ts
+++ auth/src/current-user.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
+import { currentUser } from './middlewares/current-user';
+import { requireAuth } from './middlewares/require-auth';
 
 const router = Router();
 
-router.get('/api/users/currentuser', (request, response) => {
+router.get('/api/users/currentuser', currentUser, requireAuth, (request, response) => {
     console.log('GET /api/users/currentuser');
-    response.send('GET /api/users/currentuser');
+
+    return response.send({ curentUser: request.currentUser || null });
+    
 });
 
 export { router as currentUserRouter };
